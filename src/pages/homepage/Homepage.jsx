343396834cfa40f84b894b9649bd3ed8683ea23a
satoshi-sh <satoss1108@gmail.com>
2022-12-21 22:01:24 -0600

    Implement get questions

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -2,8 +2,11 @@ import './homepage.css'
 import React, { useState, useEffect,useContext } from 'react';
 import { ThemeContext } from '../../App';
 
+const baseURL = 'https://the-trivia-api.com/api/'
+
 const Homepage = () => {
    const [categories,setCategories] = useState([])
+   const [selectedC,setSelectedC] = useState([])
    const [selectedL,setSelectedL] = useState('')
    
    const list = ['easy','medium','hard']
@@ -12,9 +15,13 @@ const Homepage = () => {
    const handleClick = (e)=>{
    if (e.target.style.backgroundColor =='white') {
       e.target.style.backgroundColor ='#23e223'
+      setSelectedC([...selectedC,e.target.innerHTML])
    }
    else{
    e.target.style.backgroundColor ='white'
+   setSelectedC(selectedC.filter(c=>{
+      return c != e.target.innerHTML
+   }))
    }
    }
    const chooseLevel = (e) => {
@@ -29,10 +36,30 @@ const Homepage = () => {
 
    const proceedQuiz = () =>{
       console.log(selectedL)
+      console.log(selectedC)
+
+      let catStr = ''
+      if (selectedC.length>0){
+      for (let i=0; i<selectedC.length; i++){
+         catStr += selectedC[i].toLowerCase().replace('&amp;','and').replaceAll(' ','_') +','
+      }
+      catStr =  'categories=' + catStr.slice(0,-1) +'&'
+      }
+      let level = ''
+      if (selectedL){
+         level ='&difficulty=' + selectedL.toLowerCase()
+      }
+      
+      const query = baseURL +'questions?' + catStr + 'limit=10' + level 
+      console.log(query)
+      fetch(query)
+      .then(res=>res.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error))
    }
    
    useEffect(()=>{
-      fetch('https://the-trivia-api.com/api/metadata')
+      fetch(baseURL + 'metadata')
       .then((res)=>res.json())
       .then((data)=>{
          setCategories(Object.keys(data.byCategory))
